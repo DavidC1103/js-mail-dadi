ecdd110ad194fd0eb7a7557ea239f2778d7a1726
David Calaiò <davidcjr11@gmail.com>
2023-03-23 14:46:21 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const askEmail = prompt('Inserisci la tua email')
+
+const listEmail = [
+    'davide@gmail.com',
+    'andrea@gmail.com',
+    'giggi@gmail.com',
+    'peppe@gmail.com',
+    'ciccio@gmail.com'
+]
