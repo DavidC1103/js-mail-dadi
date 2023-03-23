37e574adaff246b8fcfd90f876111b6119105ceb
David Calaiò <davidcjr11@gmail.com>
2023-03-23 18:01:32 +0100

    fix (for) add random in html

--- js/script.js
+++ js/script.js
@@ -9,37 +9,44 @@ const listEmail = [
 ]
 
 
+const error = document.getElementById('outputError');
+const confirm = document.getElementById('outputConfirm')
+const btn = document.getElementById('btn')
+let user = document.getElementById('tu')
+let npc = document.getElementById('npc')
+let winner = document.getElementById('winner')
+let emailTrovata = 0
 
 for(let i = 0; i < listEmail.length; i++){
     
-    const error = document.getElementById('outputError');
-    const confirm = document.getElementById('outputConfirm')
-    let user = document.getElementById('tu')
-    let npc = document.getElementById('npc')
-    
     if(inputEmail === listEmail[i]){
-
-        confirm.innerHTML = `
-        Hai accesso al gioco 
-        <br>
-        Buon divertimento!
-        `;
-
-        error.remove()
-
-        user.innerHTML = Math.floor(Math.random( ) * 7 )
-        npc.innerHTML = Math.floor(Math.random( ) * 7 )
-        
-
-    }else{
-        error.innerHTML = `
-        L'email che hai inserito non esiste nel nostro database <br>
-        Non hai accesso al gioco
-        <br>
-        Riaggiorna la pagina
-        `;
-
-        confirm.remove()
+        emailTrovata = 1    
     }
-
 }
+
+if(emailTrovata === 1){
+
+    confirm.innerHTML = `
+    Hai accesso al gioco 
+    <br>
+    Buon divertimento!
+    `;
+    
+    user.innerHTML = Math.floor(Math.random( ) * 7 )
+    npc.innerHTML = Math.floor(Math.random( ) * 7 )
+
+    if(user > npc){
+        winner.innerHTML = `TU HAI VINTO!`
+    }else{
+        winner.innerHTML = `NPC HA VINTO!`
+
+    }
+}else{
+    error.innerHTML = `
+    L'email che hai inserito non esiste nel nostro database <br>
+    Non hai accesso al gioco
+    <br>
+    Riaggiorna la pagina
+    `;
+}
+
